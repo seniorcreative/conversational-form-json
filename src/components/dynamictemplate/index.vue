194e28645831a06813b17a506d2f0ba2eee27639
Steven Smith <steven_smith@worksafe.vic.gov.au>
2020-04-28 15:26:08 +1000

    Deploy script added

--- src/components/dynamictemplate/index.vue
+++
@@ -1,3 +0,0 @@
-<template src="./src/components/dyanmictemplate.html"></template>
-<script src="./src/components/dyanmictemplate.js"></script>
-<style src="./src/components/dyanmictemplate.scss" scoped lang="scss"></style>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,30 @@
-/* // vue.config.js
 module.exports = {
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: `
-          @import "~@/assets/main.scss";
-        `
-      }
+  pluginOptions: {
+    s3Deploy: {
+      awsProfile: 'default',
+      endpoint: 'Override the default AWS endpoint with another e.g. DigitalOcean.',
+      region: 'ap-southeast-2',
+      bucket: 'conversational-form-tool',
+      createBucket: false,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: true,
+      pwaFiles: 'index.html,service-worker.js,manifest.json',
+      enableCloudfront: false,
+      cloudfrontId: 'The ID of the distribution to invalidate',
+      cloudfrontMatchers: 'A comma-separated list of paths to invalidate (default: /*)',
+      uploadConcurrency: 5,
+      cacheControl: 'Sets cache-control metadata for all uploads, overridden for individual files by pwa settings',
+      gzip: false,
+      gzipFilePattern: 'Pattern for matching files to be gzipped. (By default: \'**/*.{js,css,json,ico,map,xml,txt,svg,eot,ttf,woff,woff2}\')',
+      registry: undefined,
+      overrideEndpoint: false,
+      pluginVersion: '4.0.0-rc3'
     }
   }
 }
- */
